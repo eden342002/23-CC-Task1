d3c52cf2cd4278fafca83c938661901f97350ae6
DIVINE EPIE NGOL ESUEH <eden342002@gmail.com>
2022-07-15 20:32:38 -0500

    Update task1.tf

--- task1.tf
+++ task1.tf
@@ -1,19 +1,18 @@
 provider "aws" {
-  region = "ap-south-1"
-  profile = "23-terraform"
+  region  = "us-east-1"
 }
 
-resource "aws_instance" "web" {
-  ami           = "ami-0447a12f28fddb066"
-  instance_type = "t2.micro"
-  key_name = "23-firstkey"
-  security_groups = [ "23-Task1" ]
+resource "aws_instance" "smart2" {
+  ami             = "ami-0cff7528ff583bf9a"
+  instance_type   = "t2.micro"
+  key_name        = "Smart-Keypair1"
+  security_groups = ["launch-wizard-1"]
 
   connection {
-    type     = "ssh"
-    user     = "ec2-user"
-    private_key = file("C:/Users/Pratiksha Jain/Downloads/23-firstkey.pem")
-    host     = aws_instance.web.public_ip
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("C:/Users/eden3/Downloads/Smart-Keypair1.pem")
+    host        = aws_instance.smart2.public_ip
   }
 
   provisioner "remote-exec" {
@@ -25,76 +24,74 @@ resource "aws_instance" "web" {
   }
 
   tags = {
-    Name = "lwos1"
+    Name = "smart"
   }
 
 }
 
 
 resource "aws_ebs_volume" "esb1" {
-  availability_zone = aws_instance.web.availability_zone
+  availability_zone = aws_instance.smart2.availability_zone
   size              = 1
   tags = {
-    Name = "lwebs"
+    Name = "smart1"
   }
 }
 
 
 resource "aws_volume_attachment" "ebs_att" {
-  device_name = "/dev/sdh"
-  volume_id   = "${aws_ebs_volume.esb1.id}"
-  instance_id = "${aws_instance.web.id}"
+  device_name  = "/dev/sdh"
+  volume_id    = aws_ebs_volume.esb1.id
+  instance_id  = aws_instance.smart2.id
   force_detach = true
 }
 
 
 output "myos_ip" {
-  value = aws_instance.web.public_ip
+  value = aws_instance.smart2.public_ip
 }
 
 
-resource "null_resource" "nulllocal2"  {
-	provisioner "local-exec" {
-	    command = "echo  ${aws_instance.web.public_ip} > publicip.txt"
-  	}
+resource "null_resource" "nulllocal2" {
+  provisioner "local-exec" {
+    command = "echo  ${aws_instance.smart2.public_ip} > publicip.txt"
+  }
 }
 
 
 
-resource "null_resource" "nullremote3"  {
+resource "null_resource" "nullremote3" {
 
-depends_on = [
+  depends_on = [
     aws_volume_attachment.ebs_att,
   ]
 
 
   connection {
-    type     = "ssh"
-    user     = "ec2-user"
-    private_key = file("C:/Users/Pratiksha Jain/Downloads/23-firstkey.pem")
-    host     = aws_instance.web.public_ip
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("C:/Users/eden3/Downloads/Smart-Keypair1.pem")
+    host        = aws_instance.smart2.public_ip
   }
 
-provisioner "remote-exec" {
+  provisioner "remote-exec" {
     inline = [
       "sudo mkfs.ext4  /dev/xvdh",
       "sudo mount  /dev/xvdh  /var/www/html",
       "sudo rm -rf /var/www/html/*",
-      "sudo git clone https://github.com/Pratiksha-2305/23-CC-Task1.git /var/www/html/"
+      "sudo git clone https://github.com/eden342002/23-CC-Task1/blob/master/20220417_095651.jpg /var/www/html/"
     ]
   }
 }
 
-resource "null_resource" "nulllocal1"  {
+resource "null_resource" "nulllocal1" {
 
 
-depends_on = [
+  depends_on = [
     null_resource.nullremote3,
   ]
 
-	provisioner "local-exec" {
-	    command = "start chrome  ${aws_instance.web.public_ip}"
-  	}
+  provisioner "local-exec" {
+    command = "start chrome  ${aws_instance.smart2.public_ip}"
+  }
 }
-
-
